bed960b48edcc94a16f1557b1337f4c46de79de1
Vladimír Fojtík <fojtik@net-service.cz>
2022-09-01 10:37:50 +0200

    wip

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,7 +1,17 @@
-export interface CapacitorBgTasksPlugin {
-  setTask(options: { [key: string]: any }): Promise<{ value: string }>;
+import type { Plugin } from '@capacitor/core';
+
+interface AbstractConfig {
+  requiresCharging?: boolean;
+  requiresDeviceIdle?: boolean;
 }
 
-export interface TaskConfig {
+export interface CapacitorBgTasksPlugin extends Plugin {
+  startTask(): Promise<{ value: string }>;
+  stopTask(): Promise<{ value: string }>;
+  configure(options: { options: { [key: string]: any; } }): Promise<{ value: string }>;
+}
+
+export interface CapacitorBgTasksConfig extends AbstractConfig {
+  minimumFetchInterval?: number;
   taskId: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import type { PluginListenerHandle, PluginResultError } from '@capacitor/core';
 import { registerPlugin } from '@capacitor/core';
 
-import type { CapacitorBgTasksPlugin, TaskConfig } from './definitions';
+import type { CapacitorBgTasksPlugin, CapacitorBgTasksConfig } from './definitions';
 
 const nativePlugin = registerPlugin<CapacitorBgTasksPlugin>(
   'CapacitorBgTasks',
@@ -8,11 +9,47 @@ const nativePlugin = registerPlugin<CapacitorBgTasksPlugin>(
 );
 
 export class CapacitorBgTasks {
-  static setTask(config: TaskConfig): Promise<void> {
+  protected listener: PluginListenerHandle | null = null;
+
+  protected async init(config: CapacitorBgTasksConfig, onEvent: CallableFunction, onTimeout: CallableFunction): Promise<void> {
+    if (this.listener !== null) {
+      await this.listener.remove();
+      this.listener = null;
+    }
+
+    this.listener = await nativePlugin.addListener(config.taskId, (event:any) => {
+      if (!event.timeout) {
+        onEvent(event.taskId);
+      } else {
+        onTimeout(event.taskId);
+      }
+    });
+
+    return new Promise((resolve: CallableFunction, reject: CallableFunction) => {
+      nativePlugin.configure({ options: config }).then((result: any) => {
+        resolve(result.status);
+      }).catch((error: PluginResultError) => {
+        reject(error.message);
+      });
+    });
+  }
+
+
+  public startTask(): Promise<{ value: string }> {
     return new Promise((resolve, reject) => {
-      return nativePlugin.setTask(config).then(() => {
+      return nativePlugin.startTask().then(() => {
         resolve();
-      }).catch((error) => {
+      }).catch((error: PluginResultError) => {
+        reject(error.message);
+      });
+    });
+  }
+
+  public endTask(): Promise<{ value: string }> {
+    return new Promise((resolve, reject) => {
+      return nativePlugin.stopTask().then(() => {
+        resolve();
+      }).catch((error: PluginResultError) => {
         reject(error.message);
       });
     });
